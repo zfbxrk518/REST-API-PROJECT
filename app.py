c68ab4964bb7bc78d833b4cf58a0d399540f4ed6
yuyu <yuyuz@email.unc.edu>
2023-04-09 13:12:55 -0400

    Change endpoints of "get("/store"),post("/item"),get("/item"),get("/store/<string:store_id>"),get("/item/<string:item_id>")"

--- app.py
+++ app.py
@@ -1,58 +1,55 @@
+import uuid
 from flask import Flask, request
-
+from db import items, stores
 
 app = Flask(__name__)
 
-stores = [
-    {
-        "name": "My Store",
-        "items": [
-           {
-              "name": "Chair",
-              "price": 15.99
-           }
-        ]
-    }
-]
+
 # the first endpoint or flask route: http://127.0.0.1:5000/store
 @app.get("/store")
 def get_stores():
-    return {"stores": stores}
+    return {"stores": list(stores.values())}
 
 @app.post("/store")
 def create_store():
-    request_data = request.get_json()  # request_data is a dictionary 
-    new_store = {"name": request_data["name"], "items":[]}
-    stores.append(new_store)
-    return new_store, 201
+    store_data = request.get_json()  # request_data is a dictionary 
+    store_id = uuid.uuid4().hex
+    store = {**store_data, "id": store_id}
+    stores[store_id] = store
+    return store, 201
 
 
-@app.post("/store/<string:name>/item")  #   <string:name> is name of the dynamic url segment.The name parameter in the URL path is a variable placeholder that can capture the value of a string that is included in the URL at that position.
+@app.post("/item")  #   <string:name> is name of the dynamic url segment.The name parameter in the URL path is a variable placeholder that can capture the value of a string that is included in the URL at that position.
 def create_item(name):
-    request_data = request.get_json()
-    for store in stores:
-        if store["name"] == name:    #  name came from the url of insomnia
-            new_item = {"name": request_data["name"], "price": request_data["price"]}
-            store["items"].append(new_item)
-            return new_item, 201
-    return {"message": "Store not found"}, 404
+    item_data = request.get_json()
+    if item_data["store_id"] not in stores:
+        return {"message": "Store not found"}, 404
+    
+    item_id = uuid.uuid4().hex
+    item = {**item_data, "id": item_id}
+    items[item_id] = item
+
+    return item, 201
+
+@app.get("/item")
+def get_all_items():
+    return {"items": list(items.values())}
+
+@app.get("/store/<string:store_id>")
+def get_store(store_id):
+    try:
+        return store_id
+    except:
+        return {"message": "Store not found"}, 404
 
 
-@app.get("/store/<string:name>")
-def get_store(name):
-    for store in stores:
-        if store["name"] == name:
-            return store
-    return {"message": "Store not found"}, 404
+
+@app.get("/item/<string:item_id>")
+def get_item(item_id):
+    try:
+        return items[item_id]
+    except KeyError:
+        return {"message": "Store not found"}, 404
 
 
-
-@app.get("/store/<string:name>/item")
-def get_item_in_store(name):
-    for store in stores:
-        if store["name"] == name:
-            return {"items": store["items"]}
-    return {"message": "Store not found"}, 404
-
-@app
    
