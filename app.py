2f3afe334839198e80048220e89d067d1f107356
yuyu <yuyuz@email.unc.edu>
2023-04-09 13:52:52 -0400

    add error handling in app.py and save db.py.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import uuid
 from flask import Flask, request
+from flask_smorest import abort
 from db import items, stores
 
 app = Flask(__name__)
@@ -13,6 +14,15 @@ def get_stores():
 @app.post("/store")
 def create_store():
     store_data = request.get_json()  # request_data is a dictionary 
+    if "name" not in store_data:
+        abort(
+            400,
+            message = "Bad request. Ensure 'name' is included in the JSON payload.",
+        )
+
+    for store in stores.values():
+        if store_data["name"] == store["name"]:
+            abort(400, message=f"Store already exists")
     store_id = uuid.uuid4().hex
     store = {**store_data, "id": store_id}
     stores[store_id] = store
@@ -20,28 +30,44 @@ def create_store():
 
 
 @app.post("/item")  #   <string:name> is name of the dynamic url segment.The name parameter in the URL path is a variable placeholder that can capture the value of a string that is included in the URL at that position.
-def create_item(name):
+def create_item():
     item_data = request.get_json()
+    # Here not only we need to validata data exists.
+    # But also what type of data. Price should be a float,
+    # for example.
+    if(
+        "price" not in item_data
+        or "store_id" not in item_data
+        or "name" not in item_id
+    ):
+        abort(
+            400, 
+            message = "Bad request. Ensure 'price', 'store_id', and 'name' are included in the JSON payload."
+        )
+
+    for item in items.value():
+        if (
+            item_data["name"] == item["name"]
+            and item_data["store_id"] == item["store_id"]
+        ):
+            abort(400, message = f"Item alreay exists.")
+
     if item_data["store_id"] not in stores:
-        return {"message": "Store not found"}, 404
-    
+        abort(404, message = "Store not found")
+
     item_id = uuid.uuid4().hex
     item = {**item_data, "id": item_id}
     items[item_id] = item
 
     return item, 201
 
-@app.get("/item")
-def get_all_items():
-    return {"items": list(items.values())}
 
 @app.get("/store/<string:store_id>")
 def get_store(store_id):
     try:
         return store_id
     except:
-        return {"message": "Store not found"}, 404
-
+        abort(404, message = "Store not found")
 
 
 @app.get("/item/<string:item_id>")
@@ -49,7 +75,11 @@ def get_item(item_id):
     try:
         return items[item_id]
     except KeyError:
-        return {"message": "Store not found"}, 404
+        abort(404, message = "Item not found")
 
+
+@app.get("/item")
+def get_all_items():
+    return {"items": list(items.values())}
 
    
