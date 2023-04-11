4b0a453f14f471ce914226d2cfbbba1e22199992
yuyu <yuyuz@email.unc.edu>
2023-04-10 22:30:23 -0400

    Add some comments in the file.

--- .flaskenv
+++ .flaskenv
@@ -1,2 +1,3 @@
+# contain variables that we want flask to use when we start it up
 FLASK_APP=app
 FLASK_DEBUG=1 # automatically restart old version is FLASK_ENV=development
--- app.py
+++ app.py
@@ -1,122 +1,24 @@
-import uuid
-from flask import Flask, request
-from flask_smorest import abort
-from db import items, stores
+from flask import Flask
+from flask_smorest import Api
+
+from resources.item import blp as ItemBlueprint
+from resources.store import blp as StoreBlueprint
+
 
 app = Flask(__name__)
 
-# the first endpoint or flask route: http://127.0.0.1:5000/store
-@app.get("/store")
-def get_stores():
-    return {"stores": list(stores.values())}
+app.config["PROPAGATE_EXCEPTIONS"] = True
+app.config["API_TITLE"] = "Stores REST API"
+app.config["API_VERSION"] = "v1"
+app.config["OPENAPI_VERSION"] = "3.0.3"
+app.config["OPENAPI_URL_PREFIX"] = "/"
+app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
 
+api = Api(app)
 
-@app.get("/store/<string:store_id>")
-def get_store(store_id):
-    try:
-        return stores[store_id]
-    except KeyError:
-        abort(404, message = "Store not found")
-
-
-@app.post("/store")
-def create_store():
-    store_data = request.get_json()  # request_data is a dictionary 
-    if "name" not in store_data:
-        abort(
-            400,
-            message = "Bad request. Ensure 'name' is included in the JSON payload.",
-        )
-
-    for store in stores.values():
-        if store_data["name"] == store["name"]:
-            abort(400, message=f"Store already exists")
-
-    store_id = uuid.uuid4().hex
-    store = {**store_data, "id": store_id}
-    stores[store_id] = store
-    return store, 201
-
-
-@app.delete("/store/<string:store_id>")
-def delete_store(store_id):
-    try:
-        del stores[store_id]
-        return {"message": "Store deleted."}
-    except KeyError:
-        abort(404, message = "Store not found")
-
-
-@app.get("/item")
-def get_all_items():
-    return {"items": list(items.values())}
-
-
-@app.get("/item/<string:item_id>")
-def get_item(item_id):
-    try:
-        return items[item_id]
-    except KeyError:
-        abort(404, message = "Item not found")
-
-
-@app.post("/item")  #   <string:name> is name of the dynamic url segment.The name parameter in the URL path is a variable placeholder that can capture the value of a string that is included in the URL at that position.
-def create_item():
-    item_data = request.get_json()
-    # Here not only we need to validata data exists.
-    # But also what type of data. Price should be a float,
-    # for example.
-    if(
-        "price" not in item_data
-        or "store_id" not in item_data
-        or "name" not in item_data
-    ):
-        abort(
-            400, 
-            message = "Bad request. Ensure 'price', 'store_id', and 'name' are included in the JSON payload.",
-        )
-
-    for item in items.values():
-        if (
-            item_data["name"] == item["name"]
-            and item_data["store_id"] == item["store_id"]
-        ):
-            abort(400, message = f"Item alreay exists.")
-
-    if item_data["store_id"] not in stores:
-        abort(404, message = "Store not found")
-
-    item_id = uuid.uuid4().hex
-    item = {**item_data, "id": item_id}
-    items[item_id] = item
-
-    return item
-
-
-
-@app.delete("/item/<string:item_id>")
-def delete_item(item_id):
-    try:
-        del items[item_id]
-        return {"message": "Item deleted."}
-    except KeyError:
-        abort(404, message = "Item not found")
-
-
-@app.put("/item/<string:item_id>")
-def update_item(item_id):
-    item_data = request.get_json()
-    if "price" not in item_data or "name" not in item_data:
-        abort(400, message = "Bad request. Ensure 'price', and 'name' are included in the JSON payload.")
-
-    try:
-        item = items[item_id]
-        item |= item_data
-
-        return item
-    except KeyError:
-        abort(404, message="Item not found.")
-
+api.register_blueprint(ItemBlueprint)
+api.register_blueprint(StoreBlueprint)
 
 
 
