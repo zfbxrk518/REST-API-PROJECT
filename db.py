0a82ee62a93bbc99dd7509b64f338dc34e0515fd
yuyu <yuyuz@email.unc.edu>
2023-04-10 22:29:43 -0400

    Add some comments in the file.

--- db.py
+++ db.py
@@ -1,2 +1,6 @@
 stores = {}
 items = {}
+# use dictionaries will retrieve data like items[1]
+# connect id with the content of the item
+# so finding an item by its ID will be extremely easy if we use a dictionary for storage and not a list
+# we no longer have to iterate over the entire list to find the item that we're looking for
