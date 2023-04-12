c1caf87303233d2a6465faad243398de915a3de3
yuyu <yuyuz@email.unc.edu>
2023-04-12 12:38:38 -0400

    add marshMellow in the file

--- resources/store.py
+++ resources/store.py
@@ -1,15 +1,16 @@
 import uuid
-from flask import request
+# from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from db import stores
-
+from schemas import StoreSchema
 
 blp = Blueprint("stores", __name__, description="Operations on stores")
 
 
 @blp.route("/store/<string:store_id>")
 class Store(MethodView):
+    @blp.response(200, StoreSchema)
     def get(self,store_id):
         try:
             return stores[store_id]
@@ -24,18 +25,16 @@ class Store(MethodView):
             abort(404, message = "Store not found")
 
 
+
 @blp.route("/store")
 class StoreList(MethodView):
+    @blp.response(200, StoreSchema(many=True))
     def get(self):
-        return {"stores": list(stores.values())} # stores.values() isn't a list, list() turn it to be a list
+        return stores.values() 
     
-    def post(self):
-        store_data = request.get_json()  # request_data is a dictionary 
-        if "name" not in store_data:
-            abort(
-                400,
-                message = "Bad request. Ensure 'name' is included in the JSON payload.",
-            )
+    @blp.arguments(StoreSchema)
+    @blp.response(201, StoreSchema)
+    def post(self, store_data):
         for store in stores.values():
             if store_data["name"] == store["name"]:
                 abort(400, message=f"Store already exists")
