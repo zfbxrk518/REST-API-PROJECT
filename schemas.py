5495fb9a3e90dab3279455b2a07b8118397a07e8
yuyu <yuyuz@email.unc.edu>
2023-04-12 12:35:41 -0400

    correct the spelling issue of schema

--- schemas.py
+++ schemas.py
@@ -1,15 +1,15 @@
-# when we receive data and pass that data through Schema, it will say you can't send name through the API. It can only be used for returning data from the API.
+
 from marshmallow import Schema, fields
 
-class ItemShema(Schema):
-    id = fields.Str(dump_only=True)
+class ItemSchema(Schema):
+    id = fields.Str(dump_only=True) # It only be used for sending data back to the client.
     name = fields.Str(required=True)
     price = fields.Float(required=True)
     store_id=fields.Str(required=True)
 
 
 class ItemUpdateSchema(Schema):
-    name= fields.Str()
+    name= fields.Str()  # Optional.
     price= fields.Float()
 
 class StoreSchema(Schema):
