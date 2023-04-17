ad8560d074b2e2b4188cadff13ea423ff12b1065
yuyu <yuyuz@email.unc.edu>
2023-04-16 21:04:45 -0400

    Add itemschema and storeschema

--- schemas.py
+++ schemas.py
@@ -1,17 +1,22 @@
 
 from marshmallow import Schema, fields
 
-class ItemSchema(Schema):
+class PlainItemSchema(Schema):
     id = fields.Str(dump_only=True) # It only be used for sending data back to the client.
     name = fields.Str(required=True)
     price = fields.Float(required=True)
-    store_id=fields.Str(required=True)
 
+class PlainStoreSchema(Schema):
+    id= fields.Str(dump_only=True)
+    name= fields.Str(required=True)
 
 class ItemUpdateSchema(Schema):
     name= fields.Str()  # Optional.
     price= fields.Float()
 
-class StoreSchema(Schema):
-    id= fields.Str(dump_only=True)
-    name= fields.Str(required=True)
+class ItemSchema(PlainItemSchema):
+    store_id = fields.Int(required=True, load_only=True)
+    store = fields.Nested(PlainStoreSchema(), dump_only=True)
+
+class StoreSchema(PlainStoreSchema):
+    items = fields.List(fields.Nested(PlainItemSchema(), dump_only=True))
