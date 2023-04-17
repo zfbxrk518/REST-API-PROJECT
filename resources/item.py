34bc5189c24b7f54b6833451421c4f586b9cdfca
yuyu <yuyuz@email.unc.edu>
2023-04-17 10:32:30 -0400

    import SQLAlchemyError and ItemModel

--- resources/item.py
+++ resources/item.py
@@ -1,9 +1,12 @@
 import uuid
-# from flask import request
+from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
+from sqlalchemy.exc import SQLAlchemyError
 
-from db import items
+
+from db import db
+from models import ItemModel
 from schemas import ItemSchema, ItemUpdateSchema
 
 
@@ -14,30 +17,36 @@ blp = Blueprint("Items", "items", description="Operations on items")
 class Item(MethodView):
     @blp.response(200, ItemSchema)
     def get(self, item_id):
-        try:
-            return items[item_id]
-        except KeyError:
-            abort(404, message="Item not found.")
+        # try:
+        #     return items[item_id]
+        # except KeyError:
+        #     abort(404, message="Item not found.")
+        item = ItemModel.query.get_or_404(item_id)
+        return item
 
     def delete(self, item_id):
-        try:
-            del items[item_id]
-            return {"message": "Item deleted."}
-        except KeyError:
-            abort(404, message="Item not found.")
+        # try:
+        #     del items[item_id]
+        #     return {"message": "Item deleted."}
+        # except KeyError:
+        #     abort(404, message="Item not found.")
+        item = ItemModel.query.get_or_404(item_id)
+        raise NotImplementedError("Deleting an item is not implemented.")
 
     @blp.arguments(ItemUpdateSchema)
     @blp.response(200, ItemSchema)
     def put(self, item_data, item_id):
-        try:
-            item = items[item_id]
+        # try:
+        #     item = items[item_id]
 
-            # https://blog.teclado.com/python-dictionary-merge-update-operators/
-            item |= item_data # |= means to update the item 
+        #     # https://blog.teclado.com/python-dictionary-merge-update-operators/
+        #     item |= item_data # |= means to update the item 
 
-            return item
-        except KeyError:
-            abort(404, message="Item not found.")
+        #     return item
+        # except KeyError:
+        #     abort(404, message="Item not found.")
+        item = ItemModel.query.get_or_404(item_id)
+        raise NotImplementedError("Updating an item is not implemented.")
 
 
 
@@ -50,16 +59,23 @@ class ItemList(MethodView):
     @blp.arguments(ItemSchema)
     @blp.response(201, ItemSchema)
     def post(self, item_data):
-        for item in items.values():
-            if (
-                item_data["name"] == item["name"]
-                and item_data["store_id"] == item["store_id"]
-            ):
-                abort(400, message=f"Item already exists.")
+        # item = ItemModel(**item_data)
 
-        item_id = uuid.uuid4().hex
-        item = {**item_data, "id": item_id}
-        items[item_id] = item
-        # save the item_id to a dictionary
-        # place it in our items dictionary
+        # try:
+        #     db.session.add(item)
+        #     db.session.commit()
+        # except SQLAlchemyError:
+        #     abort(500, message="An error occured while inserting the item.")
+        # # save the item_id to a dictionary
+        # # place it in our items dictionary
+        # return item
+    
+        item = ItemModel(**item_data)
+
+        try:
+            db.session.add(item)
+            db.session.commit()
+        except SQLAlchemyError:
+            abort(500, message="An error occurred while inserting the item.")
+
         return item
