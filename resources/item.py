b78e10c1337d6d4f9fa39665f341b424cebaae31
yuyu <yuyuz@email.unc.edu>
2023-05-07 11:21:43 -0400

    docs(item): Get models by ID from the database

--- resources/item.py
+++ resources/item.py
@@ -87,25 +87,13 @@ class Item(MethodView):
 
     def delete(self, item_id):
         item = ItemModel.query.get_or_404(item_id)
-        db.session.delete(item)
-        db.session.commit()
-        return {"message": "Item deleted."}
+        raise NotImplementedError("Deleting an item is not implemented.")
 
     @blp.arguments(ItemUpdateSchema)
     @blp.response(200, ItemSchema)
     def put(self, item_data, item_id):
-        item = ItemModel.query.get(item_id)
-
-        if item:
-            item.price = item_data["price"]
-            item.name = item_data["name"]
-        else:
-            item = ItemModel(id=item_id, **item_data)
-
-        db.session.add(item)
-        db.session.commit()
-
-        return item
+        item = ItemModel.query.get_or_404(item_id)
+        raise NotImplementedError("Updating an item is not implemented.")
 
 
 @blp.route("/item")
