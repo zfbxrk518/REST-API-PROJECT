42a3ddc012a55c25f1173d85886bf126531f3978
yuyu <yuyuz@email.unc.edu>
2023-05-07 13:06:21 -0400

    Initialize tag.py

--- schemas.py
+++ schemas.py
@@ -33,6 +33,10 @@ class PlainStoreSchema(Schema):
     id = fields.Int(dump_only=True)
     name = fields.Str()
 
+class PlainTagSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str()
+
 
 class ItemSchema(PlainItemSchema):
     store_id = fields.Int(required=True, load_only=True)
@@ -47,3 +51,8 @@ class ItemUpdateSchema(Schema):
 
 class StoreSchema(PlainStoreSchema):
     items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True)
+    tags = fields.List(fields.Nested(PlainTagSchema()), dump_only=True)
+
+class TagSchema(PlainTagSchema):
+    store_id = fields.Int(load_only=True)
+    store = fields.Nested(PlainStoreSchema(), dump_only=True)
