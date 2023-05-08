6194f64618d8a0d8429b6feea399bae82d3f1360
yuyu <yuyuz@email.unc.edu>
2023-05-08 12:26:07 -0400

    Initialize item_tags

--- models/item_tags.py
+++ models/item_tags.py
@@ -3,6 +3,7 @@ from db import db
 
 class ItemsTags(db.Model):
     __tablename__ = "items_tags"
+    
 
     id = db.Column(db.Integer, primary_key=True)
     item_id = db.Column(db.Integer, db.ForeignKey("items.id"))
