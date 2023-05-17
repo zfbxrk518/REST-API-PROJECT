5c6cd65965524a287c08fa1f75505f2e657b097e
yuyu <yuyuz@email.unc.edu>
2023-05-17 11:41:32 -0400

    Add the many-to-many relationships between tags and items.

--- schemas.py
+++ schemas.py
@@ -41,6 +41,7 @@ class PlainTagSchema(Schema):
 class ItemSchema(PlainItemSchema):
     store_id = fields.Int(required=True, load_only=True)
     store = fields.Nested(PlainStoreSchema(), dump_only=True)
+    tags = fields.List(fields.Nested(PlainTagSchema()), dump_only=True)
 
 
 class ItemUpdateSchema(Schema):
@@ -55,4 +56,10 @@ class StoreSchema(PlainStoreSchema):
 
 class TagSchema(PlainTagSchema):
     store_id = fields.Int(load_only=True)
+    items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True)
     store = fields.Nested(PlainStoreSchema(), dump_only=True)
+
+class TagAndItemSchema(Schema):
+    message = fields.Str()
+    item = fields.Nested(ItemSchema)
+    tag = fields.Nested(TagSchema)
