d7fb4118c27b83c74d1d517f06112a5d6597162c
yuyu <yuyuz@email.unc.edu>
2023-05-20 13:37:07 -0400

    change the id format from string to int

--- resources/store.py
+++ resources/store.py
@@ -11,7 +11,7 @@ from schemas import StoreSchema
 blp = Blueprint("stores", __name__, description="Operations on stores")
 
 
-@blp.route("/store/<string:store_id>")
+@blp.route("/store/<int:store_id>")
 class Store(MethodView):
     @blp.response(200, StoreSchema)
     def get(self,store_id):
