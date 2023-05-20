55be96c6cb5be2a4a96e86d6fdc8b0eadb7ccf9f
yuyu <yuyuz@email.unc.edu>
2023-05-20 13:36:30 -0400

    change the id format from string to int

--- resources/item.py
+++ resources/item.py
@@ -78,7 +78,7 @@ from schemas import ItemSchema, ItemUpdateSchema
 blp = Blueprint("Items", "items", description="Operations on items")
 
 
-@blp.route("/item/<string:item_id>")
+@blp.route("/item/<int:item_id>")
 class Item(MethodView):
     @blp.response(200, ItemSchema)
     def get(self, item_id):
