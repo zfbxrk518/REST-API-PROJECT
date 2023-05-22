6b05c97e1e19b858baeb907b69b8857543b9bf43
yuyu <yuyuz@email.unc.edu>
2023-05-21 21:38:54 -0400

    add @jwt.token_in_blocklist_loader

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask_smorest import Api
 from flask_jwt_extended import JWTManager
 
 from db import db
+from blocklist import BLOCKLIST
 
 import models
 
@@ -29,11 +30,26 @@ def create_app(db_url=None):
 
     app.config["JWT_SECRET_KEY"] = "jose"
     jwt = JWTManager(app)
+    
+    @jwt.token_in_blocklist_loader
+    def check_if_token_in_blocklist(jwt_header, jwt_payload):
+        return jwt_payload["jti"] in BLOCKLIST
 
-  
 
-    app.config["JWT_SECRET_KEY"] = "jose"
-    jwt = JWTManager(app)
+    @jwt.revoked_token_loader
+    def revoked_token_callback(jwt_header, jwt_payload):
+        return (
+            jsonify(
+                {"description": "The token has been revoked.", "error": "token_revoked"}
+            ),
+            401,
+        )
+
+    @jwt.additional_claims_loader
+    def add_claims_to_jwt(identity):
+        if identity == 1:
+            return {"is_admin": True}
+        return {"is_admin": False}
 
     @jwt.expired_token_loader
     def expired_token_callback(jwt_header, jwt_payload):
