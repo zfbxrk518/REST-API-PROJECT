2610ea0f90348b86bcfc424e098c523ded622f7f
yuyu <yuyuz@email.unc.edu>
2023-05-22 14:10:34 -0400

    Error handling when a fresh token is required

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def create_app(db_url=None):
     app.config["JWT_SECRET_KEY"] = "jose"
     jwt = JWTManager(app)
     
-    @jwt.token_in_blocklist_loader
+    
     def check_if_token_in_blocklist(jwt_header, jwt_payload):
         return jwt_payload["jti"] in BLOCKLIST
 
@@ -41,6 +41,18 @@ def create_app(db_url=None):
         return (
             jsonify(
                 {"description": "The token has been revoked.", "error": "token_revoked"}
+            ),
+            401,
+        )
+    
+    @jwt.needs_fresh_token_loader
+    def token_not_fresh_callback(jwt_header, jwt_payload):
+        return (
+            jsonify(
+                {
+                    "description": "The token is not fresh.",
+                    "error": "fresh_token_required",
+                }
             ),
             401,
         )
