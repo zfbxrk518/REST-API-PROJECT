fa1757cdddc69c37283fd4304ad65d1716ce0832
yuyu <yuyuz@email.unc.edu>
2023-06-04 16:25:16 -0400

    add userSchema

--- schemas.py
+++ schemas.py
@@ -1,25 +1,3 @@
-
-# from marshmallow import Schema, fields
-
-# class PlainItemSchema(Schema):
-#     id = fields.Str(dump_only=True) # It only be used for sending data back to the client.
-#     name = fields.Str(required=True)
-#     price = fields.Float(required=True)
-
-# class PlainStoreSchema(Schema):
-#     id= fields.Str(dump_only=True)
-#     name= fields.Str(required=True)
-
-# class ItemUpdateSchema(Schema):
-#     name= fields.Str()  # Optional.
-#     price= fields.Float()
-
-# class ItemSchema(PlainItemSchema):
-#     store_id = fields.Int(required=True, load_only=True)
-#     store = fields.Nested(PlainStoreSchema(), dump_only=True)
-
-# class StoreSchema(PlainStoreSchema):
-#     items = fields.List(fields.Nested(PlainItemSchema(), dump_only=True))
 from marshmallow import Schema, fields
 
 
@@ -32,6 +10,7 @@ class PlainItemSchema(Schema):
 class PlainStoreSchema(Schema):
     id = fields.Int(dump_only=True)
     name = fields.Str()
+
 
 class PlainTagSchema(Schema):
     id = fields.Int(dump_only=True)
@@ -47,26 +26,27 @@ class ItemSchema(PlainItemSchema):
 class ItemUpdateSchema(Schema):
     name = fields.Str()
     price = fields.Float()
-    store_id = fields.Int()
 
 
 class StoreSchema(PlainStoreSchema):
     items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True)
     tags = fields.List(fields.Nested(PlainTagSchema()), dump_only=True)
 
+
 class TagSchema(PlainTagSchema):
     store_id = fields.Int(load_only=True)
     items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True)
     store = fields.Nested(PlainStoreSchema(), dump_only=True)
+
 
 class TagAndItemSchema(Schema):
     message = fields.Str()
     item = fields.Nested(ItemSchema)
     tag = fields.Nested(TagSchema)
 
+    # remove "load_only=True", I can see the hashed password in insomina. 
+    # with it, the passord part would not show in insomina
 class UserSchema(Schema):
     id = fields.Int(dump_only=True)
     username = fields.Str(required=True)
-    password = fields.Str(required=True, load_only=True) 
-    # remove "load_only=True", I can see the hashed password in insomina. 
-    # with it, the passord part would not show in insomina
+    password = fields.Str(required=True, load_only=True)
