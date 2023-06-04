cfd738f643b698f906f9884dcd54d43af31fd6a4
yuyu <yuyuz@email.unc.edu>
2023-06-04 16:59:58 -0400

    change the store_id from string to int

--- resources/store.py
+++ resources/store.py
@@ -1,5 +1,3 @@
-import uuid
-from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from sqlalchemy.exc import SQLAlchemyError, IntegrityError
@@ -8,32 +6,22 @@ from db import db
 from models import StoreModel
 from schemas import StoreSchema
 
-blp = Blueprint("stores", __name__, description="Operations on stores")
+
+blp = Blueprint("Stores", "stores", description="Operations on stores")
 
 
 @blp.route("/store/<int:store_id>")
 class Store(MethodView):
     @blp.response(200, StoreSchema)
-    def get(self,store_id):
-        # try:
-        #     return stores[store_id]
-        # except KeyError:
-        #     abort(404, message = "Store not found")
+    def get(self, store_id):
         store = StoreModel.query.get_or_404(store_id)
         return store
 
-    def delete(self,store_id):
-        # try:
-        #     del stores[store_id]
-        #     return {"message": "Store deleted."}
-        # except KeyError:
-        #     abort(404, message = "Store not found")
+    def delete(self, store_id):
         store = StoreModel.query.get_or_404(store_id)
-        # raise NotImplementedError("Deleting a store is not implemented.")
         db.session.delete(store)
         db.session.commit()
         return {"message": "Store deleted"}, 200
-
 
 
 @blp.route("/store")
@@ -41,23 +29,10 @@ class StoreList(MethodView):
     @blp.response(200, StoreSchema(many=True))
     def get(self):
         return StoreModel.query.all()
-    
+
     @blp.arguments(StoreSchema)
     @blp.response(201, StoreSchema)
     def post(self, store_data):
-        # for store in stores.values():
-        #     if store_data["name"] == store["name"]:
-        #         abort(400, message=f"Store already exists")
-
-        # store_id = uuid.uuid4().hex  # uuid is unique identifier, a more common way instead of <string: name>
-        # store = {**store_data, "id": store_id}
-        # # **passing keyword arguments to a construture, 
-        # # unpack the values in the store_data dictionary and include them in the new {**store_data, "id": store_id} dictionary,
-        # # add the id key as a seperate key within the dictionary
-        # stores[store_id] = store  # save it to the database
-       
-        # return store
-
         store = StoreModel(**store_data)
         try:
             db.session.add(store)
@@ -71,4 +46,3 @@ class StoreList(MethodView):
             abort(500, message="An error occurred creating the store.")
 
         return store
-            
