35727b41d364c3da54c5174330380a64104189ff
yuyu <yuyuz@email.unc.edu>
2023-06-04 16:59:01 -0400

    change the item_id from string to int

--- resources/item.py
+++ resources/item.py
@@ -1,75 +1,5 @@
-# import uuid
-# from flask import request
-# from flask.views import MethodView
-# from flask_smorest import Blueprint, abort
-# from sqlalchemy.exc import SQLAlchemyError
-
-
-# from db import db
-# from models import ItemModel
-# from schemas import ItemSchema, ItemUpdateSchema
-
-
-# blp = Blueprint("Items", "items", description="Operations on items")
-
-
-# @blp.route("/item/<string:item_id>")
-# class Item(MethodView):
-#     @blp.response(200, ItemSchema)
-#     def get(self, item_id):
-#         # try:
-#         #     return items[item_id]
-#         # except KeyError:
-#         #     abort(404, message="Item not found.")
-#         item = ItemModel.query.get_or_404(item_id)
-#         return item
-
-#     def delete(self, item_id):
-#         # try:
-#         #     del items[item_id]
-#         #     return {"message": "Item deleted."}
-#         # except KeyError:
-#         #     abort(404, message="Item not found.")
-#         # item = ItemModel.query.get_or_404(item_id)
-#         # raise NotImplementedError("Deleting an item is not implemented.")
-#         db.session.delete(item)
-#         db.session.commit()
-#         return {"message": "Item deleted."}
-
-#     @blp.arguments(ItemUpdateSchema)
-#     @blp.response(200, ItemSchema)
-#     def put(self, item_data, item_id):
-#         # try:
-#         #     item = items[item_id]
-
-#         #     # https://blog.teclado.com/python-dictionary-merge-update-operators/
-#         #     item |= item_data # |= means to update the item 
-
-#         #     return item
-#         # except KeyError:
-#         #     abort(404, message="Item not found.")
-#         # item = ItemModel.query.get_or_404(item_id)
-#         # raise NotImplementedError("Updating an item is not implemented.")
-#         item = ItemModel.query.get(item_id)
-#         if item:
-#             item.price = item_data["price"]
-#             item.name = item_data["name"]
-#         else:
-#             item = ItemModel(id=item_id, **item_data)
-
-#         db.session.add(item)
-#         db.session.commit()
-
-#         return item
-
-
-# @blp.route("/item")
-# class ItemList(MethodView):
-#     @blp.response(200, ItemSchema(many=True))
-#     def get(self):
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
-from flask_jwt_extended import jwt_required, get_jwt
 from sqlalchemy.exc import SQLAlchemyError
 
 from db import db
@@ -81,21 +11,13 @@ blp = Blueprint("Items", "items", description="Operations on items")
 
 @blp.route("/item/<int:item_id>")
 class Item(MethodView):
-    @jwt_required()
     @blp.response(200, ItemSchema)
     def get(self, item_id):
         item = ItemModel.query.get_or_404(item_id)
         return item
 
-    @jwt_required()
     def delete(self, item_id):
-        jwt = get_jwt()
-        if not jwt.get("is_admin"):
-            abort(401, message="Admin privilege required.")
-
-
         item = ItemModel.query.get_or_404(item_id)
-        # raise NotImplementedError("Deleting an item is not implemented.")
         db.session.delete(item)
         db.session.commit()
         return {"message": "Item deleted."}
@@ -103,8 +25,8 @@ class Item(MethodView):
     @blp.arguments(ItemUpdateSchema)
     @blp.response(200, ItemSchema)
     def put(self, item_data, item_id):
-
         item = ItemModel.query.get(item_id)
+
         if item:
             item.price = item_data["price"]
             item.name = item_data["name"]
@@ -116,14 +38,13 @@ class Item(MethodView):
 
         return item
 
+
 @blp.route("/item")
 class ItemList(MethodView):
-    @jwt_required()
     @blp.response(200, ItemSchema(many=True))
     def get(self):
         return ItemModel.query.all()
 
-    @jwt_required(fresh=True)
     @blp.arguments(ItemSchema)
     @blp.response(201, ItemSchema)
     def post(self, item_data):
