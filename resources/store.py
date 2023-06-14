d77af2a20c1f95addcacbe52a4ca2e14dbc8d508
yuyu <yuyuz@email.unc.edu>
2023-06-13 23:38:24 -0400

    rewrite the code

--- resources/store.py
+++ resources/store.py
@@ -1,3 +1,51 @@
+# from flask.views import MethodView
+# from flask_smorest import Blueprint, abort
+# from sqlalchemy.exc import SQLAlchemyError, IntegrityError
+
+# from db import db
+# from models import StoreModel
+# from schemas import StoreSchema
+
+
+# blp = Blueprint("Stores", "stores", description="Operations on stores")
+
+
+# @blp.route("/store/<int:store_id>")
+# class Store(MethodView):
+#     @blp.response(200, StoreSchema)
+#     def get(self, store_id):
+#         store = StoreModel.query.get_or_404(store_id)
+#         return store
+
+#     def delete(self, store_id):
+#         store = StoreModel.query.get_or_404(store_id)
+#         db.session.delete(store)
+#         db.session.commit()
+#         return {"message": "Store deleted"}, 200
+
+
+# @blp.route("/store")
+# class StoreList(MethodView):
+#     @blp.response(200, StoreSchema(many=True))
+#     def get(self):
+#         return StoreModel.query.all()
+
+#     @blp.arguments(StoreSchema)
+#     @blp.response(201, StoreSchema)
+#     def post(self, store_data):
+#         store = StoreModel(**store_data)
+#         try:
+#             db.session.add(store)
+#             db.session.commit()
+#         except IntegrityError:
+#             abort(
+#                 400,
+#                 message="A store with that name already exists.",
+#             )
+#         except SQLAlchemyError:
+#             abort(500, message="An error occurred creating the store.")
+
+#         return store
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from sqlalchemy.exc import SQLAlchemyError, IntegrityError
@@ -10,7 +58,7 @@ from schemas import StoreSchema
 blp = Blueprint("Stores", "stores", description="Operations on stores")
 
 
-@blp.route("/store/<int:store_id>")
+@blp.route("/store/<string:store_id>")
 class Store(MethodView):
     @blp.response(200, StoreSchema)
     def get(self, store_id):
@@ -46,3 +94,4 @@ class StoreList(MethodView):
             abort(500, message="An error occurred creating the store.")
 
         return store
+
